b0a608cf9856034b2ad848fdd10a78a27e9371f3
kikenky <aaronhdezalv@hotmail.com>
2020-11-15 12:53:46 +0000

    SEGUNDA VERSIÓN KATA4:
    CAMBIOS: Se implementa en la clase main el modelo IPO mediante la adición de los métodos execute(), input() y output()

--- src/kata4/main/Kata4.java
+++ src/kata4/main/Kata4.java
@@ -8,14 +8,33 @@ import kata4.model.Mail;
 import kata4.model.Histogram;
 import java.util.List;
 
+
 public class Kata4 {
     private static List<Mail> mailList;
     private static Histogram <String> mailHistogram;
     
     public static void main(String[] args) {
-        String fileName = new String("./files/emails.txt");
+       execute(); 
+    }
+    
+    private static void execute() {
+        input();
+        process();
+        output();
+    }
+    
+    private static void input() {
+        String fileName = new String("emails.txt");
         mailList = MailListReader.read(fileName);
+    }
+    
+    private static void process() {
         mailHistogram = MailHistogramBuilder.build(mailList);
+
+    }
+    
+    private static void output() {
         new HistogramDisplay("HISTOGRAMA",mailHistogram).execute();
     }
+    
 }
